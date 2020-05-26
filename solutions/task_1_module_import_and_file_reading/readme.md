a6d390f79fdac56bb27dde40b14c77fe06f3e573
Tjark Smalla <tjark.smalla@gmail.com>
2020-05-26 16:49:26 +0200

    add overview

--- solutions/task_1_module_import_and_file_reading/readme.md
+++ solutions/task_1_module_import_and_file_reading/readme.md
@@ -16,12 +16,12 @@ python3 task_1_1 extra.py
 ```
 Als Ergebnis sollte das Programm dir auf deiner Kommandozeile die Worte 'Hello World!' ausgeben - der Klassiker.
 Nahezu jedes erste Programm eines Entwicklers in einer neuen Sprache gibt diese Worte aus. Simpler kann ein Programm nicht sein.
-Wenn das nicht geklappt hat, weiß der Entwickler (in diesem Fall du), dass ein Fehler mit der Installation gibt und der Fehler nicht in dem Programm zu suchen ist. 
+Wenn das nicht geklappt hat, weiß der Entwickler (in diesem Fall du), dass es ein Fehler mit der Installation gibt und der Fehler nicht in dem Programm zu suchen ist. 
 
 > (i) Info: Du wirst dich vielleicht wundern, warum Programmierer in ihren Dateinamen nie Leerzeichen benutzen. 
 > Versuche das Programm task_1_1 extra.py auszuführen. Du wirst merken, dass das nicht ohne weiteres klappt. Der Computer kann nicht unterscheiden, ob das Leerzeichen zum Dateinamen gehört, oder ob du ihm weitere Anweisungen geben willst.
 > Das Problem lässt sich zwar umgehen (z.B. mit `python3 "task_1_1 extra.py"), da das aber nur Umstände macht und eine Fehlerquelle ist vermeidet man Leerzeichen einfach.
 
-## Task 1.2 - Grundlegene Syntax 
+## Task 1.2 - Grundlegene Syntax
 
 ## Task 1.3 - PYTHONPATH, Unterprogramme und 
---
+++ solutions/task_1_module_import_and_file_reading/task_1_2/task_1_2_overview.py
@@ -0,0 +1,57 @@
+# Hier soll dir ein Überblick über die grundlegendsten Syntax Eigenschaften von Python gegeben werden.
+# Du kannst immer wieder hier hin zurückkehren um verschiedene Operationen nachzuschlagen.
+# Variablen wurden ja schon in Task 1.1 eingeführt. Sie können die Werte verschiedener Datentype beeinhalten.
+# Die wichtigsten Datentypen in Python sind Nummern, Strings, Listen, Dicts und Funktionen.
+# Funktionen behandeln wir in einem späteren Kapitel noch einmal genauer.
+
+# Nummern
+# ----------------------------------------------------------------------------------------------------------------------
+number_1 = 3
+number_2 = 5.5
+number_3 = number_1 + number_2  # Simple Addition
+number_4 = number_1 - number_2  # Simple Subtraktion
+
+# Strings
+# ----------------------------------------------------------------------------------------------------------------------
+string_1 = 'Hello World!'
+
+# Listen
+# ----------------------------------------------------------------------------------------------------------------------
+list_1 = []  # Liste ohne Inhalt
+list_2 = [number_1]  # Liste mit einem Element
+# Liste mit zwei Element unterschiedlicher Datentypen. In viele Programmiersprachen wäre das gar nicht möglich!
+# In der Regel ist das allerdings auch keine gute Idee, da es so leicht zu Fehlern bei der Verarbeitung der Liste führen kann.
+list_3 = [number_3, string_1]
+list_4 = [number_1, number_2]
+list_5 = [number + 1 for number in list_4] # Dieses Konsturkt nennt sich 'List Comprehension'. In diesem Fall wird jeder Nummer aus list_4 mit 1 addiert. Die Werte in list_4 bleiben unverändert! Nur list_5 enthält die addierten Werte
+
+# Dicts - Die Kurzform von Dictionary
+# ----------------------------------------------------------------------------------------------------------------------
+# Dicts kannst du benutzen um Daten zusammenzufassen. Du kannst in ihnen Werte unter einem beliebigen String ablegen.
+# Wie in einem Nachschlagewerk eben. Diese Werte können alle Datentypen haben. Du kannst sie also auch schachteln.
+# In anderen Programmiersprachen werden diese Konstrukte auch oft Map oder Record bezeichnet.
+
+dict_1 = {}
+dict_2 = {'Eine Nummer': number_1}
+dict_3 = {'eins bis 3': [1, 2, 3]}
+dict_4 = {'geschachtelt': {             # geschachteltes Dictionary mit einem Wert aus einem anderen Dict
+    'Eins': dict_2['Eine Nummer']}
+}
+
+# Hier werden alle Werte auf der Kommandozeile ausgegeben
+# ----------------------------------------------------------------------------------------------------------------------
+print(number_1)
+print(number_2)
+print(number_3)
+print(number_3)
+
+print(string_1)
+
+print(list_1)
+print(list_2)
+print(list_3)
+
+print(dict_1)
+print(dict_2)
+print(dict_3)
+print(dict_4)
