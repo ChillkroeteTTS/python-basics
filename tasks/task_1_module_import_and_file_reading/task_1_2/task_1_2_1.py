0abe01e487cc20cdf3e039b70e2f94c91d105527
Tjark Smalla <tjark.smalla@gmail.com>
2020-05-27 09:35:11 +0200

    wip

--- tasks/task_1_module_import_and_file_reading/task_1_2/task_1_2_1.py
+++ tasks/task_1_module_import_and_file_reading/task_1_2/task_1_2_1.py
@@ -5,13 +5,13 @@
 # In Programmiersprachen wird bei solch Funktionen zwischen zwei Verhaltensarten unterschieden: exclusive und inclusive.
 # Im wesentlichen beschreibt es nur, ob das angegebene Zahlenintervall den Start oder wie hier Endpunkt mit einbezieht 0-101 oder eben nicht (0-100).
 # Pythons range Funktion verhält siuch also exclusive.
-eins_bis_hundert = range(0, 101)
+null_bis_hundert = range(0, 101)
 
 # Der folgende Teil enthält eine sogenannte Schleife. Er gibt alle Zahlen von 0-100 aus, die glatt durch 2 teilbar sind.
 # All der Code, der eingerückt ist, wird für jedes Element einzeln ausgeführt. Also 100 mal in diesem Fall.
 # Codeblöcke werden in Python durch die Einrückung markiert. Verrückst du den untenstehenden COde also wird er nicht mehr funktionieren.
 # Andere Programmiersprachen lösen das gleiche Problem oft mit den Klammern {} anstatt durch die Einrückung.
-for number in eins_bis_hundert:
+for number in null_bis_hundert:
     # Eine If-Abfrage steuert den Programmablauf. Wenn das Ergebnis hinter dem if True ergibt, wird der erste Block ausgeführt. Andernfalls der Block hinter dem else.
     if number % 2 == 0: # Der Modulo Operator % berechnet den Rest einer Division. 2.5 % 2 ergibt also 0.5.
         print(number)
@@ -21,7 +21,7 @@ for number in eins_bis_hundert:
 # Gib alle durch 2 teilbaren Zahlen von 0 bis -100 aus, ohne die range Methode zu benutzen!
 # Verbotene Elemente: range()
 # ==================================
-for number in eins_bis_hundert:
+for number in null_bis_hundert:
 # ==================================
     if number % 2 == 0:
         print(number)
